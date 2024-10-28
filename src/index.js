561ae237cc8dd15fcf0ee0a730708838c9978b59
Shubham Maurya <shubhammaurya0923@gmail.com>
2024-10-28 22:21:45 +0530

    Added First Utility function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { DB_CONNECT } from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 DB_CONNECT()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is Running on PORT : ${"3000"}`);
+    });
+  })
+  .catch((error) => console.error(error));
